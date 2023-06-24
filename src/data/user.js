678aa0046e8242dfafdd14ff4973a65dd5d80003
SammySmash613 <36828580+SammySmash613@users.noreply.github.com>
2023-06-23 20:08:28 -0700

    Initial commit

---
+++ src/data/user.js
@@ -0,0 +1,56 @@
+const INFO = {
+	main: {
+		title: "Sammy Paykel",
+		name: "Sammy Paykel",
+		email: "sameerpaykel@gmail.com",
+		logo: "../sp-logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/spaykel",
+		linkedin: "https://www.linkedin.com/in/sameerpaykel/"
+	},
+
+	homepage: {
+		title: "Hi! I'm Sammy Paykel",
+		description:
+			"I am a computer science student at Cal Poly in San Luis Obispo. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a student and as a software developer.",
+	},
+
+	about: {
+		title: "Diligent. Dauntless. Dynamic.",
+		description:
+			"placeholder",
+	},
+
+	projects: [
+		{
+			title: "Fraternity Mobile App",
+			description:
+				"Collaborated with fraternity brothers to create a custom mobile application, built on a React Native w/ Typescript framework. Github repository below",
+			logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+			linkText: "View Project",
+			link: "https://github.com/sigma-phi-delta-beta-nu/sigma-phi-delta-mobile-app",
+		},
+
+		{
+			title: "Chess AI",
+			description:
+				"Utilized Tensorflow library to train AI models in Chess strategy. Implemented Monte Carlo Tree Search predictions for move selection. Github repository below.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/keonroohparvar/SPD_Chessbot",
+		},
+
+		{
+			title: "Personal Portfolio Website",
+			description:
+				"The website you're looking at now!",
+			logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
